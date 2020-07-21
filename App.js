4aaafc705e63ada02ac70ddbcf6188f607af3a39
mohinimraut <mohiniraut7585@gmail.com>
2020-07-21 12:21:45 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react'
+import { mapping, light as lightTheme } from '@eva-design/eva'
+import { ApplicationProvider, IconRegistry } from 'react-native-ui-kitten'
+import { EvaIconsPack } from '@ui-kitten/eva-icons'
+
+import Firebase, { FirebaseProvider } from './src/utils'
+import TabNavigator from './src/navigation/Tabnavigator'
+
+const App = () => (
+  <Fragment>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider mapping={mapping} theme={lightTheme}>
+      <FirebaseProvider value={Firebase}>
+        <TabNavigator/>
+      </FirebaseProvider>
+    </ApplicationProvider>
+  </Fragment>
+)
+
+export default App
